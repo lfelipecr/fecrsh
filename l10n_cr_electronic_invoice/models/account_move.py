eaf3d8fd99600bd8fe6aa9314712601408234fd4
Erick Abrego <programador2@grupoley.com>
2023-11-22 10:35:29 -0600

    Update account_move.py
    PRUEBAS DE MONTOS

--- l10n_cr_electronic_invoice/models/account_move.py
+++ l10n_cr_electronic_invoice/models/account_move.py
@@ -215,6 +215,7 @@ class AccountInvoice(models.Model):
             is_tax = "taxed" if line['line'].tax_ids else "no_taxed"
             if exo:
                 amount = round(self.exoneration_cal() * round(line['monto_total'], digits), digits)
+                raise ValidationError(f"Monto total: {line['monto_total']}, Monto con exoneración: {amount}, Exoneración : {self.exoneration_cal()}")
                 amounts[line_type + "_" + is_tax] += round(amount,digits)
                 amounts[line_type + "_exempt"] += round(line['monto_total'] - amount, digits)
             else:
